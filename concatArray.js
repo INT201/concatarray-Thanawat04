17af820d38f7b6de52c4a0abfd120a30984c7f52
Thanawat Chen <thanawat.th@mail.kmutt.ac.th>
2022-09-08 00:28:41 +0700

    v2

--- concatArray.js
+++ concatArray.js
@@ -1,13 +1,13 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1,array2) {
-    if(array1===null||undefined&&array2===null||undefined)
+    if(array1===null||array1===undefined&&array2===null||array2===undefined)
       return undefined
-    else if(array1===null||undefined||[])
+    else if(array1===null||array1===undefined||array1===[])
       return array2 
-    else if(array2===null||undefined||[])
+    else if(array2===null||array2===undefined||array2===[])
       return array1
-    else if((array1===[])&&(array2===[]))
+    else if(array1===[]&&array2===[])
       return array1
     else
       return array1.concat(array2)
